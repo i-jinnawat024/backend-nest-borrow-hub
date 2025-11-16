fa0c7a3c26ecac498968d484f2fa4025dc5ea9eb
Jinnawat inyim <i.jinnawat024@gmail.com>
2025-11-16 23:17:34 +0700

    feat(): cors

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,25 +1,13 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 
+import { buildCorsOptions } from './common/config/cors.config';
 import { HttpExceptionFilter } from './common/shared/filters/http-exception.filter';
 import { ResponseTransformInterceptor } from './common/shared/interceptors/response-transform.interceptor';
 
 export function configureApp(app: INestApplication): void {
   app.setGlobalPrefix('api');
-  const env = process.env.NODE_ENV || 'development';
-   const rawOrigins =
-     env === 'production'
-       ? process.env.CORS_ORIGIN_PROD
-       : process.env.CORS_ORIGIN_DEV;
+  app.enableCors(buildCorsOptions());
 
-   app.setGlobalPrefix('api');
-    app.enableCors({
-    origin: [rawOrigins, 'http://localhost:4200'],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
-
- 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -28,6 +16,7 @@ export function configureApp(app: INestApplication): void {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+
   app.useGlobalInterceptors(new ResponseTransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 }
